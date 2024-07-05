752f59106f7a1ae8bd02e631825316a23cf23c2b
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-07-05 20:10:21 +0200

    add pull down to refresh

--- androidApp/src/main/java/me/tbandawa/android/aic/android/ui/screens/ArtworksScreen.kt
+++ androidApp/src/main/java/me/tbandawa/android/aic/android/ui/screens/ArtworksScreen.kt
@@ -2,21 +2,27 @@ package me.tbandawa.android.aic.android.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
+import app.cash.paging.compose.collectAsLazyPagingItems
 import me.tbandawa.android.aic.android.ui.composables.ArtworksToolbar
 import me.tbandawa.android.aic.android.ui.composables.ItemArtwork
 import me.tbandawa.android.aic.android.ui.composables.LoadingData
@@ -28,7 +34,7 @@ import me.tbandawa.android.aic.remote.responses.Artwork
 import org.koin.androidx.compose.inject
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun ArtworksScreen(
     navigateToArtwork: (artworkId: Int) -> Unit
@@ -44,43 +50,63 @@ fun ArtworksScreen(
             rememberTopAppBarState()
         )
         val pagingItems: LazyPagingItems<Artwork> = viewModel.pagedArtworks.collectAsLazyPagingItems()
+        val pullRefreshState = rememberPullRefreshState(
+            refreshing = pagingItems.loadState.refresh is LoadState.Loading && pagingItems.itemSnapshotList.size > 0,
+            onRefresh = {
+                pagingItems.refresh()
+            }
+        )
 
         Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            modifier = Modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
+                .pullRefresh(pullRefreshState),
             containerColor = Color.White,
             topBar = {
                 ArtworksToolbar(title = "Art Institute of Chicago", scrollBehavior = scrollBehavior)
             },
-        ) {
-            LazyColumn(
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .padding(it)
-                    .fillMaxSize()
+        ) { paddingValues ->
+            Box(
+                Modifier
+                    .pullRefresh(pullRefreshState)
+                    .padding(paddingValues)
             ) {
-                items(pagingItems.itemCount) { index ->
-                    ItemArtwork(artwork = pagingItems[index]!!) { artworkId ->
-                        navigateToArtwork(artworkId)
-                    }
-                }
-                pagingItems.apply {
-                    when {
-                        loadState.refresh is LoadState.Loading -> {
-                            item { LoadingData() }
-                        }
-                        loadState.refresh is LoadState.Error -> {
-                            val error = pagingItems.loadState.refresh as LoadState.Error
-                            item { LoadingDataError(message = error.error.message!!) { retry() } }
-                        }
-                        loadState.append is LoadState.Loading -> {
-                            item { LoadingMore() }
-                        }
-                        loadState.append is LoadState.Error -> {
-                            val error = pagingItems.loadState.append as LoadState.Error
-                            item { LoadingMoreError(message = error.error.message!!) { retry() } }
+                LazyColumn(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(pagingItems.itemCount) { index ->
+                        ItemArtwork(artwork = pagingItems[index]!!) { artworkId ->
+                            navigateToArtwork(artworkId)
                         }
                     }
+                    pagingItems.apply {
+                        when {
+                            loadState.refresh is LoadState.Loading -> {
+                                item { LoadingData() }
+                            }
+                            loadState.refresh is LoadState.Error -> {
+                                val error = pagingItems.loadState.refresh as LoadState.Error
+                                item { LoadingDataError(message = error.error.message!!) { retry() } }
+                            }
+                            loadState.append is LoadState.Loading -> {
+                                item { LoadingMore() }
+                            }
+                            loadState.append is LoadState.Error -> {
+                                val error = pagingItems.loadState.append as LoadState.Error
+                                item { LoadingMoreError(message = error.error.message!!) { retry() } }
+                            }
+                        }
+                    }
                 }
+                PullRefreshIndicator(
+                    refreshing = pagingItems.loadState.refresh is LoadState.Loading && pagingItems.itemSnapshotList.size > 0,
+                    state = pullRefreshState,
+                    scale = true,
+                    modifier = Modifier
+                        .align(alignment = Alignment.TopCenter)
+                )
             }
         }
     }
