7c73c90a831cdf7a435fc44c327fe41ef44b7de0
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-11-22 23:59:27 +0200

    add message to layout

--- androidApp/src/main/java/me/tbandawa/android/aic/android/ui/navigation/NavGraph.kt
+++ androidApp/src/main/java/me/tbandawa/android/aic/android/ui/navigation/NavGraph.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.LifecycleStartEffect
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -86,8 +90,16 @@ fun NavGraph() {
                 .height(if (!connectivityManager.isNetworkAvailable.value) 45.dp else 0.dp)
                 .fillMaxWidth()
                 .height(45.dp)
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-
+            Text(
+                text = "No Internet Connection",
+                color = Color.White,
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
         }
 
     }
